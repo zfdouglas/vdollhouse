5020165b4d35b7f3ad4d18433cc121be77695090
zfdouglas <zacharyfdouglas@gmail.com>
2023-02-11 13:44:21 -0500

    feat: added process steps and styling

--- src/ProcessBar/ProcessTiles.tsx
+++ src/ProcessBar/ProcessTiles.tsx
@@ -1,16 +1,31 @@
+import { CardContent, CardMedia, Typography } from "@mui/material";
+import Card from "@mui/material/Card";
 import React from "react";
+import theme from "../Common/ThemeProvider";
+
 interface ProcessTileProps {
   process: string;
-  image: React.ReactElement;
+  image: JSX.Element;
   blurb: string;
 }
 const ProcessTile = ({ process, image, blurb }: ProcessTileProps) => {
   return (
-    <div>
-      <h4>{process}</h4>
-      {image}
-      <div>{blurb}</div>
-    </div>
+    <Card sx={{ maxWidth: "17%" }}>
+      <CardContent>
+        <Typography
+          variant="h5"
+          component="div"
+          color={theme.palette.primary.main}
+          gutterBottom
+        >
+          {process}
+        </Typography>
+        <CardMedia sx={{ height: "60%" }} title="green iguana">
+          {image}
+        </CardMedia>
+        <div>{blurb}</div>
+      </CardContent>
+    </Card>
   );
 };
 export default ProcessTile;
