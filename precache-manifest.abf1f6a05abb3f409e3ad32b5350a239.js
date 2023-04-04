cbd78acbaaa9f14b0f1d431d5e7d7c4874be384c
zfdouglas <zacharyfdouglas@gmail.com>
2023-04-04 19:42:03 -0400

    Updates

---
+++ precache-manifest.abf1f6a05abb3f409e3ad32b5350a239.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8b236d73474ba7d2b68",
+    "url": "/vdollhouse/static/css/main.1822eaef.chunk.css"
+  },
+  {
+    "revision": "c8b236d73474ba7d2b68",
+    "url": "/vdollhouse/static/js/main.3e81f1b9.chunk.js"
+  },
+  {
+    "revision": "46c8ad0f43a90684a12c",
+    "url": "/vdollhouse/static/js/runtime~main.454c8ba0.js"
+  },
+  {
+    "revision": "692a652f85ec2d458a41",
+    "url": "/vdollhouse/static/js/2.a496936c.chunk.js"
+  },
+  {
+    "revision": "1ba5a6ca5687f6e0c2bf",
+    "url": "/vdollhouse/static/js/3.4e3714ee.chunk.js"
+  },
+  {
+    "revision": "aee7850d9f3e5de1534c1249a5acdcba",
+    "url": "/vdollhouse/static/media/green2.aee7850d.jpg"
+  },
+  {
+    "revision": "138d1ebe4f371268bd758ff3c386bdb0",
+    "url": "/vdollhouse/static/media/TeamPlaceholder.138d1ebe.jpg"
+  },
+  {
+    "revision": "cbf5f54399675d6a685267d47e0fdbbf",
+    "url": "/vdollhouse/static/media/logogreen.cbf5f543.png"
+  },
+  {
+    "revision": "7056d27b25ac454f455d0d189d7dea3f",
+    "url": "/vdollhouse/static/media/interior.7056d27b.jpg"
+  },
+  {
+    "revision": "f088b2c641fa5c28c6c64dd22bcd254c",
+    "url": "/vdollhouse/static/media/zfd.f088b2c6.jpg"
+  },
+  {
+    "revision": "b12f71683bc37a4e14dd542e83c5e369",
+    "url": "/vdollhouse/static/media/grey.b12f7168.jpg"
+  },
+  {
+    "revision": "16b82e6b2e00d926b068986c856924f1",
+    "url": "/vdollhouse/static/media/wood.16b82e6b.jpg"
+  },
+  {
+    "revision": "3d1de70a114b05b38fd1ac5ebe5f07a7",
+    "url": "/vdollhouse/index.html"
+  }
+];
